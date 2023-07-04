d95a3f623edf0a498e59a78f16d395cf58114ca9
Ben Albuquerque <38185704+paulooliveiraal@users.noreply.github.com>
2023-07-03 23:26:23 -0300

    Update GerenciadorCarros.java

--- src/GerenciadorCarros.java
+++ src/GerenciadorCarros.java
@@ -1,6 +1,6 @@
 public class GerenciadorCarros {
     public static void main(String[] args) {
 
-        Veiculos a1 = new Veiculos("aaa0001","Shevroler", "UNO", 2020);
+        Veiculos a1 = new Veiculos("aaa0001","FIAT", "UNO", 2020, true);
     }
 }

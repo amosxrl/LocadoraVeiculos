b22870525e26e1ff9357967a4ea2f862f830a864
Amós Matias da Silva <amosxrl@outlook.com>
2023-07-03 23:29:32 -0300

    teste

--- src/GerenciadorCarros.java
+++ src/GerenciadorCarros.java
@@ -1,4 +1,5 @@
 public class GerenciadorCarros {
+    //Arquivo feito pra teste
     public static void main(String[] args) {
 
         Veiculos a1 = new Veiculos("aaa0001","FIAT", "UNO", 2020, true);

8102952ba2fe1f7e3cd80923d4d8a5d6b68fffc5
Amós Matias da Silva <amosxrl@outlook.com>
2023-07-22 15:44:03 -0300

    Create ClienteService.java

---
+++ src/service/ClienteService.java
@@ -0,0 +1,8 @@
+package service;
+
+public class ClienteService {
+    public Cliente obterClientePorLocacao(int idLocacao) {
+        // Lógica para obter o cliente com base no ID da locação
+        // ...
+        return cliente;
+}
